f77f83640cb283f7df9060bbd2c5c2aa65369192
Anna Butkevych <anna.butkevych@nure.ua>
2025-05-24 06:49:52 +0300

    Create header.php

---
+++ Lab3/smp-pzpi-23-5-butkevych-anna-lab3/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="uk">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+<head>
+    <meta charset="UTF-8">
+    <title>Магазин</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="header header-content container">
+    <a class="logo" href="index.php">Web-Магазин</a>
+    <nav>
+        <ul class="nav-menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="shop.php">Products</a></li>
+            <li><a href="cart.php">Cart</a></li>
+        </ul>
+    </nav>
+</div>
